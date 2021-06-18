c2b1cd3fffa32534ee4a155eac19be5192369338
noto <suisui898@gmail.com>
2021-06-18 10:29:17 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #coding: utf-8
 from ir_analyzer  import ir
 #確認用
-ir_model = ir.IR_main(maximum_symbol_num = 1, num_of_symbols = 8)
+ir_model = ir.IR_main(maximum_symbol_num = 3, num_of_symbols = 16)
 symbols, symbol_startnotes, onset, pitch, duration, beat, closure_index = ir_model.ir_main("data/turk.xml")
 #ir_model.plot_ir(0, 50, symbols, pitch)
 #ir_model.plot_ir(0, 50, symbol_startnotes, pitch)
